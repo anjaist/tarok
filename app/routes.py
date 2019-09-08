3319b905e27e2e865eb16dddf8f3a121665cc383
Anja Istenic <aistenic1@gmail.com>
2019-09-08 15:07:40 +0200

    creating new user in db on sign-up page

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from functools import wraps
 from flask import url_for, session, redirect, request, render_template, Blueprint
 
 import config
+from app.models import User, db
 
 bp = Blueprint('routes', __name__)
 
@@ -13,7 +14,7 @@ def login_required(f):
         if session['logged_in']:
             return f(*args, **kwargs)
         else:
-            return redirect(url_for('login'))
+            return redirect(url_for('routes.login'))
     return login_required_wrap
 
 
@@ -32,24 +33,43 @@ def login():
 
 @bp.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    # TODO: implement check for uniqueness of username and email
     error = None
     if request.method == 'POST':
-        if request.form['password'] != request.form['password2']:
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        repeat_password = request.form['password2']
+
+        if password != repeat_password:
             error = 'Gesla se ne ujemata.'
         else:
-            session['logged_in'] = True
-            return redirect(url_for('play'))
+            try:
+                insert_user_into_db(username, email, password)
+                session['logged_in'] = True
+                return redirect(url_for('routes.play'))
+            except Exception as e:
+                return str(e)
+
     return render_template('sign-up.html', error=error)
 
 
 @bp.route('/logout')
 def logout():
     session['logged_in'] = False
-    return redirect(url_for('login'))
+    return redirect(url_for('routes.login'))
 
 
 @bp.route('/play')
 @login_required
 def play():
     return render_template('play.html')
+
+
+def insert_user_into_db(username, email, password):
+    user = User(
+        username=username,
+        email=email,
+        password=password  # TODO: encrypt
+    )
+    db.session.add(user)
+    db.session.commit()

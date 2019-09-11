89fa43b8d9e1c875a1f05cf918beacb97f271b6d
Anja Istenic <aistenic1@gmail.com>
2019-09-11 14:57:04 +0200

    added setup for heroku

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 import config
 from config import POSTGRES
@@ -22,9 +23,12 @@ def create_app():
 
     return app
 
-if __name__ == '__main__':
-    app = create_app()
 
+app = create_app()
+migrate = Migrate(app, db)
+
+
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 

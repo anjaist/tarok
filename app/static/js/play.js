ef50f796752a7f2f4add8d8d51ac0a10a3e9e1e7
Anja Istenic <aistenic1@gmail.com>
2019-10-25 20:36:46 +0200

    using protocol from js instead of hardcoded

--- app/static/js/play.js
+++ app/static/js/play.js
@@ -1,4 +1,4 @@
-let socket = io.connect('http://' + document.domain + ':' + location.port);
+let socket = io.connect(window.location.protocol + '//' + document.domain + ':' + location.port);
 
 socket.on('connect', function() {
     console.log('=> Websocket connected! <=');

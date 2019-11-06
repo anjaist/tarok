c3d07a059768cd9ec74c651f544030ce7219a14a
Anja Istenic <aistenic1@gmail.com>
2019-11-06 21:42:27 +0100

    fix for cards sorting and better placement of opponent's cards

--- app/game_utils.py
+++ app/game_utils.py
@@ -1,7 +1,7 @@
 from random import shuffle
 
 SUITS = ['hearts', 'spades', 'clubs', 'diamonds']
-SUIT_CARDS = ['14-king', '13-queen', '12-caval', '11-jack', '10', '9', '8', '7']
+SUIT_CARDS = ['aa-king', 'bb-queen', 'cc-caval', 'dd-jack', 'ee', 'ff', 'gg', 'hh']
 TAROK_CARDS = [str(i) for i in range(1, 23)]
 
 def get_deck() -> list:
@@ -14,7 +14,7 @@ def get_deck() -> list:
 
 
 def deal_new_round(usernames: list) -> dict:
-    """Each player gets either 16 or 12 random cards from the deck. 6 random cards are asigned to talon"""
+    """Each player gets either 16 or 12 random cards from the deck. 6 random cards are assigned to talon"""
     cards_per_player = 12 if len(usernames) == 4 else 16
     deck = get_deck()
     shuffle(deck)
@@ -25,6 +25,9 @@ def deal_new_round(usernames: list) -> dict:
         player_cards = []
         for _ in range(cards_per_player):
             player_cards.append(deck.pop())
+        print('------')
+        print(f'player cards: {player_cards}')
+        print(f'sorted: {sort_player_cards(player_cards)}')
         dealt[player] = sort_player_cards(player_cards)
 
     dealt['talon'] = [deck.pop() for _ in range(6)]
@@ -35,12 +38,14 @@ def deal_new_round(usernames: list) -> dict:
 
 def sort_player_cards(unsorted_cards: list) -> list:
     """Sorts cards dealt to each player, where hearts are at the far left,
-    followed by clubs, taroks, diamonds and clubs. The suits are sorted in desc order and the taroks in asc"""
+    followed by spades, taroks, diamonds and clubs. The suits are sorted in desc order and the taroks in asc"""
+    suits = SUITS[:2] + ['tarok'] + SUITS[2:]
     sorted_cards = []
-    for suit_name in SUITS:
-        suit = [x for x in unsorted_cards if suit_name in x]
-        sorted_cards.extend(sorted(suit, reverse=True))
+    for suit_name in suits:
+        if suit_name == 'tarok':
+            suit = [int(x) for x in unsorted_cards if x.isdigit()]
+        else:
+            suit = [x for x in unsorted_cards if suit_name in x]
+        sorted_cards.extend(sorted(suit))
 
-    taroks = [x for x in unsorted_cards if x.isdigit()]
-
-    return sorted_cards[:2] + sorted(taroks) + sorted_cards[2:]
+    return sorted_cards

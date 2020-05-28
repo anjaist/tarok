3d50e5fb334fff5b885589fcceff956473e7b433
Anja Istenic <aistenic1@gmail.com>
2020-05-28 12:49:49 +0200

    determining which cards from hand can be played

--- app/game_utils.py
+++ app/game_utils.py
@@ -54,7 +54,55 @@ def sort_player_cards(unsorted_cards: list) -> list:
     return sorted_cards
 
 
+def is_card_tarok(card: str) -> bool:
+    """determines if a card is a tarok"""
+    return card.isdigit()
+
+
+def get_taroks_in_hand(cards_in_hand: list) -> list:
+    """returns only tarok hands found in cards_in_hand"""
+    return [card for card in cards_in_hand if is_card_tarok(card)]
+
+
+def get_card_suit(card: str) -> str:
+    """returns the suit of a card in hand"""
+    if is_card_tarok(card):
+        raise RuntimeError("Can't extract the suit of a tarok card.")
+    return card.split('-')[-1]
+
+
+def get_cards_of_suit(suit: str, cards_in_hand: list) -> list:
+    """returns all cards in cards_in_hand that are of the suit specified"""
+    return [card for card in cards_in_hand if suit in card]
+
+
 def get_possible_card_plays(cards_on_table: list, cards_in_hand: list) -> list:
     """returns a list of cards that can be played based on what is already on the table"""
-    # TODO
-    return ['temp-card-1', 'temp-card-2', 'temp-card-3']
+    taroks_in_hand = get_taroks_in_hand(cards_in_hand)
+
+    # any card can be played if no card is already on the table
+    if not cards_on_table:
+        return cards_in_hand
+
+    # if XXI and XXII are played in that order, pagat must be played third (if in hand)
+    if len(cards_on_table) == 2 and cards_on_table[0] == 21 and cards_on_table[1] == 22:
+        if 1 in cards_in_hand:
+            return [1]
+
+    # if the card on the table is a tarok, a tarok must be played
+    if is_card_tarok(cards_on_table[0]):
+        if not taroks_in_hand:  # if there is no tarok in hand, any card can be played
+            return cards_in_hand
+        return taroks_in_hand
+
+    on_table_suit = get_card_suit(cards_on_table[0])
+
+    # a card of the same suit must be played
+    cards_of_suit = get_cards_of_suit(on_table_suit, cards_on_table)
+
+    # if there is no cards of the same suit in hand, a tarok must be played
+    if not cards_of_suit:
+        if not taroks_in_hand:  # if there is no tarok in hand, any card can be played
+            return cards_in_hand
+        return taroks_in_hand
+    return cards_of_suit
